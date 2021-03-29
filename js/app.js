d7613e914f50d4a345e0f82bbb96dbd5e9f44ce4
maryam alnajjar <mariam.mohannad98@gmail.com>
2021-03-29 17:30:48 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -8,15 +8,15 @@ toLowerCase();
 // console.log(age);
 // tolowerCase();
 if (age==='yes' || age==='y'){
-    console.log('Ooooh pravoo');
+    // console.log('Ooooh pravoo');
     alert('Ooooh pravoo');
 }
 else if (age==='no' || age==='n'){
-    console.log('Nooo Iam 22');
+    // console.log('Nooo Iam 22');
     alert('Nooo I am 22 ');
 }
 else{
-    console.log('Please only say yes or no');
+    // console.log('Please only say yes or no');
     alert('Please only say yes or no');
 }
 let adu= prompt(' Am i studied engineering? ');
@@ -26,15 +26,15 @@ toLowerCase();
 // console.log(adu);
 // tolowerCase();
 if (adu==='yes' || adu==='y'){
-    console.log('Ooooh Nooo, Iam studied business economic');
+    // console.log('Ooooh Nooo, Iam studied business economic');
     alert('OOoooh Nooo, Iam studied business economic');
 }
 else if (adu==='no' || adu==='n'){
-    console.log('Yess, Iam studied business economic');
+    // console.log('Yess, Iam studied business economic');
     alert('Yes, Iam studied business economic');
 }
 else{
-    console.log('Please only say yes or no');
+    // console.log('Please only say yes or no');
     alert('Please only say yes or no');
 }
 let uni= prompt(' Am i studied in The University of Jordan? ');
@@ -44,15 +44,15 @@ toLowerCase();
 // console.log(uni);
 // tolowerCase();
 if (uni==='yes' || uni==='y'){
-    console.log('Good job thats right');
+    // console.log('Good job thats right');
     alert('Good job thats right');
 }
 else if (uni==='no' || uni==='n'){
-    console.log('NO, I am studied in The University of Jordan, dont worry you can do better ');
+    // console.log('NO, I am studied in The University of Jordan, dont worry you can do better ');
     alert('NO, I am studied in The University of Jordan, dont worry you can do better');
 }
 else{
-    console.log('Please only say yes or no');
+    // console.log('Please only say yes or no');
     alert('Please only say yes or no');
 }
 let food= prompt('Is the burger my favorite fast food? ');
@@ -62,15 +62,15 @@ toLowerCase();
 // console.log(food);
 // tolowerCase();
 if (food==='yes' || food==='y'){
-    console.log('Good job thats right, I love burger soo much');
+    // console.log('Good job thats right, I love burger soo much');
     alert('Good job thats right, I love burger soo much');
 }
 else if (food==='no' || food==='n'){
-    console.log('NO, not again, i think burger is most delicious food ');
+    // console.log('NO, not again, i think burger is most delicious food ');
     alert('NO, not again, i think burger is most delicious food ');
 }
 else{
-    console.log('Please only say yes or no');
+    // console.log('Please only say yes or no');
     alert('Please only say yes or no');
 }
 let night= prompt('Am I Love night more than morning?? ');
@@ -80,15 +80,15 @@ toLowerCase();
 // console.log(night);
 // tolowerCase();
 if (night==='yes' || night==='y'){
-    console.log('Pravoo, I like the night but i dont like dark circles, Hahahaha');
+    // console.log('Pravoo, I like the night but i dont like dark circles, Hahahaha');
     alert('Pravoo, I like the night but i dont like dark circles, Hahahaha');
 }
 else if (night==='no' || night==='n'){
-    console.log('NO, i know its not healthy but i love the night');
+    // console.log('NO, i know its not healthy but i love the night');
     alert('NO, i know its not healthy but i love the night ');
 }
 else{
-    console.log('Please only say yes or no');
+    // console.log('Please only say yes or no');
     alert('Please only say yes or no');
 }
 alert('I hope you enjoied with me ' + userName + ' know see more information about me ')
